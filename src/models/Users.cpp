c91af120298d8580405c22b6c3b0ce5b881d0c66
Biptaste <biptaste@gmail.com>
2011-01-08 18:30:53 +0100

    Try to make check login with binary hash.

--- src/models/Users.cpp
+++ src/models/Users.cpp
@@ -12,13 +12,14 @@ namespace models {
  */
 Users::Users(cppdb::session sqliteDb) : SqliteModel(sqliteDb) {
     // TODO ADD check for the username 
-    check_passwd_state = sqliteDb.create_prepared_statement("SELECT 1 FROM users2 WHERE password = ?");
+    check_passwd_state = sqliteDb.create_prepared_statement("SELECT 1 FROM users2 WHERE username = ? AND password = ?");
 }
 
 /**
  *
  */
 void Users::check_login(std::string login, std::string pass) {
+    try {
     std::auto_ptr<cppcms::message_digest> d(cppcms::message_digest::md5());
     
     char buf[16];
@@ -26,6 +27,22 @@ void Users::check_login(std::string login, std::string pass) {
     d->readout(buf);
 
     std::stringstream in(buf);
+    
+    /*
+    TEST add user plop
+
+    cppdb::session sql("sqlite3:db=../doc/sqlite3.db");
+
+    cppdb::statement stat;
+    stat = sql << 
+            "INSERT INTO users2(id, username, password) "
+            "VALUES(?,?,?)";
+            //<< 100 << "plop" << in;
+    stat.bind(100);
+    stat.bind("plop");
+    stat.bind(in);
+    stat.exec();
+    */
 
     check_passwd_state.bind(login);
     check_passwd_state.bind(in);
@@ -33,15 +50,20 @@ void Users::check_login(std::string login, std::string pass) {
    
    
     // TODO replace me by a real piece of code 
-    std::cout <<buf <<  std::endl;
+    std::cout << in.str() <<  std::endl;
     int checkresult = 0;
-    res.fetch(0,checkresult);
+    bool tip = false;
+    tip = res.fetch(0,checkresult);
+    std::cout << res.empty();
     if (checkresult != 1 ) {
         std::cout << "wrong password" << std::endl; 
     } else {
-        std::cout << "right one" << std::endl; 
+        std::cout << "right one" << std::endl;
     }
-
+    // Don't forget to reset
+    check_passwd_state.reset();
+    } catch (std::exception const &e)
+    {std::cerr << "ERROR: " << e.what() << std::endl;}
 
 }
 
