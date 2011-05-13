e60b542c29a23433bb749f69249cc662c6548502
SIMON Allan <allan.simon@supinfo.com>
2011-05-13 11:16:14 +0800

    added some comments in the add sentence form

--- src/contents/forms/add_sentence.h
+++ src/contents/forms/add_sentence.h
@@ -1,3 +1,28 @@
+/**
+ * Tatoeba Project, free collaborative creation of multilingual corpuses project
+ * Copyright (C) 2011 Allan SIMON <allan.simon@supinfo.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @category Tatoebacpp
+ * @package  Forms
+ * @author   Allan SIMON <allan.simon@supinfo.com>
+ * @license  Affero General Public License
+ * @link     http://tatoeba.org
+ */
+
 #ifndef TATOEBACPP_ADD_SENTENCE_FORM_H
 #define TATOEBACPP_ADD_SENTENCE_FORM_H
 
@@ -7,13 +32,27 @@
 using namespace cppcms;
 
 namespace forms {
-
+    
+    /**
+     * @struct AddSentence
+     * form to add a new sentence in the database
+     */
     struct AddSentence : public cppcms::form {
+        /**
+         * The sentence text itself
+         */
         widgets::text sentenceString;
+        /**
+         * Select to choose the language in which the sentence
+         * is written
+         */
         widgets::select sentenceLang;
         widgets::submit submit;        
     
         // init of the form
+        /**
+         * Constructor
+         */
         AddSentence() {
             *this + sentenceString + sentenceLang + submit;
 
